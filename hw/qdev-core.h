8b00a823bc3ecce2e96fa7c56af9bc4fdc771331
Liu Ping Fan <pingfank@linux.vnet.ibm.com>
2012-12-05 14:44:29 +0100

    introduce qdev_unplug_complete() to remove device from views
    When device unplug has been ack by guest, we first remove it from memory
    to prevent incoming access from dispatcher. Then we isolate it from
    device composition tree
    
    Signed-off-by: Vasilis Liaskovitis <vasilis.liaskovitis@profitbricks.com>

--- hw/qdev-core.h
+++ hw/qdev-core.h
@@ -38,6 +38,7 @@ typedef struct DeviceClass {
 
     /* callbacks */
     void (*reset)(DeviceState *dev);
+    void (*unmap)(DeviceState *dev);
 
     /* device state */
     const struct VMStateDescription *vmsd;
@@ -144,6 +145,7 @@ void qdev_init_nofail(DeviceState *dev);
 void qdev_set_legacy_instance_id(DeviceState *dev, int alias_id,
                                  int required_for_version);
 void qdev_unplug(DeviceState *dev, Error **errp);
+void qdev_unplug_complete(DeviceState *dev, Error **errp);
 void qdev_free(DeviceState *dev);
 int qdev_simple_unplug_cb(DeviceState *dev);
 void qdev_machine_creation_done(void);
