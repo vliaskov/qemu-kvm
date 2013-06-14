17121d6473c9e650de21aef1776399d61996c069
Amos Kong <akong@redhat.com>
2013-06-14 15:45:52 +0800

    net: add support of mac-programming over macvtap in QEMU side
    Currently macvtap based macvlan device is working in promiscuous
    mode, we want to implement mac-programming over macvtap through
    Libvirt for better performance.
    
    Design:
     QEMU notifies Libvirt when rx-filter config is changed in guest,
     then Libvirt query the rx-filter information by a monitor command,
     and sync the change to macvtap device. Related rx-filter config
     of the nic contains main mac, rx-mode items and vlan table.
    
    This patch adds a QMP event to notify management of rx-filter change,
    and adds a monitor command for management to query rx-filter
    information.
    
    Test:
     If we repeatedly add/remove vlan, and change macaddr of vlan
     interfaces in guest by a loop script.
    
    Result:
     The events will flood the QMP client(management), management takes
     too much resource to process the events.
    
     Event_throttle API (set rate to 1 ms) can avoid the events to flood
     QMP client, but it could cause an unexpected delay (~1ms), guests
     guests normally expect rx-filter updates immediately.
    
     So we use a flag for each nic to avoid events flooding, the event
     is emitted once until the query command is executed. The flag
     implementation could not introduce unexpected delay.
    
    There maybe exist an uncontrollable delay if we let Libvirt do the
    real change, guests normally expect rx-filter updates immediately.
    But it's another separate issue, we can investigate it when the
    work in Libvirt side is done.
    
    Michael S. Tsirkin: tweaked to enable events on start.
    Michael S. Tsirkin: fixed not to crash when no id.
    
    Signed-off-by: Amos Kong <akong@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- net/net.c
+++ net/net.c
@@ -961,6 +961,53 @@ void print_net_client(Monitor *mon, NetClientState *nc)
                    nc->info_str);
 }
 
+RxFilterInfoList *qmp_query_rx_filter(bool has_name, const char *name,
+                                      Error **errp)
+{
+    NetClientState *nc;
+    RxFilterInfoList *filter_list = NULL, *last_entry = NULL;
+
+    QTAILQ_FOREACH(nc, &net_clients, next) {
+        RxFilterInfoList *entry;
+        RxFilterInfo *info;
+
+        /* only query rx-filter information of nic */
+        if (nc->info->type != NET_CLIENT_OPTIONS_KIND_NIC) {
+            continue;
+        }
+        if (has_name && strcmp(nc->name, name) != 0) {
+            continue;
+        }
+
+        if (nc->info->query_rx_filter) {
+            info = nc->info->query_rx_filter(nc);
+            entry = g_malloc0(sizeof(*entry));
+            entry->value = info;
+
+            if (!filter_list) {
+                filter_list = entry;
+            } else {
+                last_entry->next = entry;
+            }
+            last_entry = entry;
+        } else if (has_name) {
+            error_setg(errp, "net client(%s) doesn't support"
+                       " rx-filter querying", name);
+            break;
+        }
+    }
+
+    if (filter_list == NULL && !error_is_set(errp)) {
+        if (has_name) {
+            error_setg(errp, "invalid net client name: %s", name);
+        } else {
+            error_setg(errp, "no net client supports rx-filter querying");
+        }
+    }
+
+    return filter_list;
+}
+
 void do_info_network(Monitor *mon, const QDict *qdict)
 {
     NetClientState *nc, *peer;
